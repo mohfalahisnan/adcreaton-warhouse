d7f6d2bf515ea99518155a92a9f3cabc9207030a
lilfall <m.falah.isnan@gmail.com>
2024-04-23 16:25:53 +0700

    vreate user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import LogoutButton from "@/components/ui/LogoutButton";
 import { auth } from "./auth";
 import { LoginButton } from "@/components/ui/LoginButton";
+import CreateUser from "@/components/CreateUser";
 
 export default async function Home() {
   const session = await auth();
   console.log(session);
   return (
-    <div>
+    <div className="w-full h-screen flex items-center justify-center gap-4">
       <LoginButton />
       {session && <LogoutButton />}
+      <CreateUser />
     </div>
   );
 }
