06a05510d126472143a9de2e1399083cfce86ed5
alfina <alfina@akupintar.id>
2024-10-04 08:11:07 +0700

    cancel edit

--- components/WarehouseListSetting.tsx
+++ components/WarehouseListSetting.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { getWarehouses } from "@/lib/actions/warehouse";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -11,8 +11,11 @@ import {
   TableRow,
 } from "./ui/table";
 import { Button } from "./ui/button";
+import Condition from "./Condition";
 
 const WarehouseListSetting = () => {
+  const [edit, setEdit] = useState(false);
+  const [selected, setSelected] = useState<number>();
   const query = useQuery({
     queryKey: ["warehouses"],
     queryFn: getWarehouses,
@@ -20,31 +23,52 @@ const WarehouseListSetting = () => {
   return (
     <div>
       {query.isLoading && <h3>Loading...</h3>}
-      <Table>
-        <TableHeader className="bg-accent">
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Location</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {query.data?.map((item) => {
-            return (
-              <TableRow key={item.warehouse_id}>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.location}</TableCell>
-                <TableCell className="flex items-center justify-center gap-2">
-                  <Button size={"xs"}>Edit</Button>
-                  <Button size={"xs"} variant={"destructive"}>
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+      <Condition show={!edit}>
+        <Table>
+          <TableHeader className="bg-accent">
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Location</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {query.data?.map((item) => {
+              return (
+                <TableRow key={item.warehouse_id}>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.location}</TableCell>
+                  <TableCell className="flex items-center justify-center gap-2">
+                    <Button
+                      size={"xs"}
+                      onClick={() => {
+                        setEdit(true);
+                        setSelected(item.warehouse_id);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                    <Button size={"xs"} variant={"destructive"}>
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Condition>
+
+      <Condition show={edit}>
+        <Button
+          onClick={() => {
+            setSelected(undefined);
+            setEdit(false);
+          }}
+        >
+          Cancel
+        </Button>
+      </Condition>
     </div>
   );
 };
